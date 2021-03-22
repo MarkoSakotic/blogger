03637316cab22dbda3857d69f097732c0b4e53d8
MarkoSakotic <marcodeee@gmail.com>
2021-03-22 01:30:25 +0100

    added site-wide sidebar

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_21_224755) do
+ActiveRecord::Schema.define(version: 2021_03_21_235455) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -53,6 +53,14 @@ ActiveRecord::Schema.define(version: 2021_03_21_224755) do
 
   create_table "tags", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "email"
+    t.string "login"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
